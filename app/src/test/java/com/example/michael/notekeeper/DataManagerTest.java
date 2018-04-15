e9c6f357865f8fcb5d18b5970133a6f018c436db
Michael Hartley <mtjhartley@gmail.com>
2018-04-15 16:17:12 -0700

    added some more unit testing

---
+++ app/src/test/java/com/example/michael/notekeeper/DataManagerTest.java
@@ -0,0 +1,58 @@
+package com.example.michael.notekeeper;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetUp() throws Exception {
+        sDataManager = DataManager.getInstance();
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+        sDataManager.getNotes().clear();
+        sDataManager.initializeExampleNotes();
+    }
+
+    @Test
+    public void createNewNote() {
+        final CourseInfo course = sDataManager.getCourse("android_async");;
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body text of my test note";
+
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
+
+        //can just use the method name thanks to the static import!
+        assertEquals(newNote.getCourse(), course);
+        assertEquals(newNote.getTitle(), noteTitle);
+        assertEquals(newNote.getText(), noteText);
+    }
+
+    @Test
+    public void createNewNoteOneStepCreation() {
+        final CourseInfo course = sDataManager.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of my test note";
+
+        int noteIndex = sDataManager.createNewNote(course, noteTitle, noteText);
+
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
+
+        assertEquals(compareNote.getCourse(), course);
+        assertEquals(compareNote.getTitle(), noteTitle);
+        assertEquals(compareNote.getText(), noteText);
+    }
+}
